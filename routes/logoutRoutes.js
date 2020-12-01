1e654ce0289e2b492fea0045d018c44c67b5c84a
EduardoMedina <emedina@alset.com.mx>
2020-12-01 11:44:51 -0600

    Updated logout confirmation

---
+++ routes/logoutRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const User = require('../schemas/UserSchema');
+
+router.get('/', (req, res, next) => {
+  if (req.session) {
+    req.session.destroy(() => {
+      res.redirect('/login');
+    });
+  }
+});
+
+module.exports = router;
